48dce0275bbb53f7ae784a8c0e5503e75b7bb41e
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2026-04-09 18:50:36 +0100

    Added new capacitor things, added navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,25 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "../components/ThemeContext";
 
 import Gateway from "./pages/Gateway";
-
 import Home from "./pages/Welcome";
-
 import Restaurant from "./pages/Restaurant";
 import HiCafe from "./pages/restaurant/HiCafe";
 import Breakfast from "./pages/restaurant/Breakfast";
 import CafeFiesta from "./pages/restaurant/CafeFiesta";
 import Locations from "./pages/restaurant/Locations";
-
 import HotelActivities from "./pages/HotelActivities";
-
 import Roomkey from "./pages/Roomkey";
-
 import Settings from "./pages/Settings";
 import Appearance from "./pages/settings/Appearance";
 import Accessibility from "./pages/settings/Accessibility";
 import Apps from "./pages/settings/Apps";
 import About from "./pages/settings/About";
 import Privacy from "./pages/settings/Privacy";
-
 import Help from "./pages/HelpCenter";
-
 import FullscreenViewer from "./pages/FullscreenViewer";
+
+// Import the named exports instead of a default export
+import { BottomNav, SideRail } from "../components/Navigation";
 
 export default function App() {
   return (
@@ -33,30 +29,27 @@ export default function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={<Gateway />} />
-
           <Route path="/welcome" element={<Home />} />
-
           <Route path="/restaurant" element={<Restaurant />} />
           <Route path="/restaurant/hicafe" element={<HiCafe />} />
           <Route path="/restaurant/breakfast" element={<Breakfast />} />
           <Route path="/restaurant/cafefiesta" element={<CafeFiesta />} />
           <Route path="/restaurant/locations" element={<Locations />} />
-
           <Route path="/hotelactivities" element={<HotelActivities />} />
-
           <Route path="/roomkey" element={<Roomkey />} />
-
           <Route path="/settings" element={<Settings />} />
           <Route path="/settings/appearance" element={<Appearance />} />
           <Route path="/settings/accessibility" element={<Accessibility />} />
           <Route path="/settings/apps" element={<Apps />} />
           <Route path="/settings/about" element={<About />} />
           <Route path="/settings/privacy" element={<Privacy />} />
-
           <Route path="/help" element={<Help />} />
-
           <Route path="/viewer" element={<FullscreenViewer />} />
         </Routes>
+
+        {/* Render both navigation components */}
+        <BottomNav />
+        <SideRail />
       </HashRouter>
     </ThemeProvider>
   );
